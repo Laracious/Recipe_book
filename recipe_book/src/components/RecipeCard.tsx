a43026ce7d8518e338f2ea767ec9bbfaebaea63a
Omar <omarkhaled201148@gmail.com>
2023-12-19 03:49:01 +0200

    optimizing skeletons

--- recipe_book/src/components/RecipeCard.tsx
+++ recipe_book/src/components/RecipeCard.tsx
@@ -1,23 +1,23 @@
-import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import { Recipe } from "../Hooks/useRecipes";
-import CriticScore from "./CriticScore";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react"
+import { Recipe } from "../Hooks/useRecipes"
+import CriticScore from "./CriticScore"
 
 interface Props {
-  recipe: Recipe;
+    recipe: Recipe
 }
 
-const RecipeCard = ({ recipe }: Props) => {
+const RecipeCard = ({  recipe } : Props) => {
   return (
-    <Card maxW="sm" borderRadius={10} overflow="hidden">
-      <Image src={recipe.thumbnail_url} />
-      <CardBody>
-        <Heading>{recipe.name}</Heading>
-        <HStack spacing={124}>
-          <CriticScore  scoring={(recipe.user_ratings.score * 10).toFixed(1)} />
-        </HStack>
-      </CardBody>
+    <Card width = '300px'maxW="sm" borderRadius={10} overflow="hidden">
+        <Image src={recipe.thumbnail_url}/>
+        <CardBody>
+            <Heading fontSize="xl">{recipe.name}</Heading>
+            <HStack spacing={124}>
+             <CriticScore scoring={(recipe.user_ratings.score * 10).toFixed(1)}/>
+            </HStack>
+        </CardBody>
     </Card>
-  );
-};
+  )
+}
 
-export default RecipeCard;
+export default RecipeCard  

c86cdcdac6e885aa345f651ad75e62ebe3265917
Laracious <omolaraamoke22@gmail.com>
2023-12-21 13:14:08 -0600

    completed Sign-up and Sign-in including API call

--- recipe_book/src/App.tsx
+++ recipe_book/src/App.tsx
@@ -14,8 +14,7 @@ export interface RecipeQuery {
 function App() {
   const [recipeQuery, setRecipeQuery] =useState<RecipeQuery>({} as RecipeQuery)
   return (
-    //<<<<<<< HEAD
-    <div>
+<div>
       <Grid
         templateColumns="repeat(5, 1fr)"
         gap={0}
@@ -24,46 +23,80 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav">
-          <NavBar onSearch={(searchText) => setRecipeQuery({ ...recipeQuery, })}/>
-          <Routes>
+        <Routes>
+          <Route path="/" element={
+            <>
+            <GridItem area="nav">
+            <NavBar onSearch={(searchText) => setRecipeQuery({ ...recipeQuery, })}/>
+            </GridItem>
+             <Show above="lg">
+             <GridItem area="aside" padding="10px" margin="100px 0 0 200px">
+               Aside
+             </GridItem>
+           </Show>
+           <GridItem area="main">
+             <RecipeGrid />
+           </GridItem>
+         </>
+          } />
             <Route path="/Sign-up" element={<SignUp />} />
             <Route path="/Sign-in" element={<SignIn />} />
           </Routes>
-        </GridItem>
-        <Show above="lg">
-          <GridItem area="aside" padding="10px" margin="100px 0 0 200px">
-            Aside
-          </GridItem>
-        </Show>
-        <GridItem area="main">
-          <RecipeGrid />
-        </GridItem>
       </Grid>
     </div>
+
+
+
+    //<<<<<<< HEAD
+    // <div>
+    //   <Grid
+    //     templateColumns="repeat(5, 1fr)"
+    //     gap={0}
+    //     templateAreas={{
+    //       base: `"nav " " main"`,
+    //       lg: `"nav nav" "aside main"`,
+    //     }}
+    //   >
+    //     <GridItem area="nav">
+    //       <NavBar onSearch={(searchText) => setRecipeQuery({ ...recipeQuery, })}/>
+    //       <Routes>
+    //         <Route path="/Sign-up" element={<SignUp />} />
+    //         <Route path="/Sign-in" element={<SignIn />} />
+    //       </Routes>
+    //     </GridItem>
+    //     <Show above="lg">
+    //       <GridItem area="aside" padding="10px" margin="100px 0 0 200px">
+    //         Aside
+    //       </GridItem>
+    //     </Show>
+    //     <GridItem area="main">
+    //       <RecipeGrid />
+    //     </GridItem>
+    //   </Grid>
+    // </div>
   );
   //=======
 
-  <Grid
-    templateAreas={{
-      base: `"nav " " main"`,
-      lg: `"nav nav" "aside main"`,
-    }}
-  >
-    <GridItem area="nav">
-      <NavBar />
-    </GridItem>
-    <Show above="lg">
-      <GridItem area="aside">Aside</GridItem>
-    </Show>
-    <GridItem area="main">
-      <RecipeGrid />
-    </GridItem>
-    <Routes>
-      <Route path="/Sign-up" element={<SignUp />} />
-      <Route path="/Sign-in" element={<SignIn />} />
-    </Routes>
-  </Grid>;
+  // <Grid
+  //   templateAreas={{
+  //     base: `"nav " " main"`,
+  //     lg: `"nav nav" "aside main"`,
+  //   }}
+  // >
+  //   <GridItem area="nav">
+  //     <NavBar />
+  //   </GridItem>
+  //   <Show above="lg">
+  //     <GridItem area="aside">Aside</GridItem>
+  //   </Show>
+  //   <GridItem area="main">
+  //     <RecipeGrid />
+  //   </GridItem>
+  //   <Routes>
+  //     <Route path="/Sign-up" element={<SignUp />} />
+  //     <Route path="/Sign-in" element={<SignIn />} />
+  //   </Routes>
+  // </Grid>;
 
   //>>>>>>> 6acb0b2e2e708f4dee60172bf28ed2224f88798d
 }

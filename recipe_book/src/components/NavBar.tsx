55fcbd7dc0c01eae080ae76f64c6ed1f884edb12
Omar <omarkhaled201148@gmail.com>
2023-12-28 10:25:28 +0200

    changes to navbar and the button

--- recipe_book/src/components/NavBar.tsx
+++ recipe_book/src/components/NavBar.tsx
@@ -8,12 +8,12 @@ interface Props {
   onSearch: (searchText: string) => void;
 }
 
-const NavBar = ({ onSearch}: Props) => {
+const NavBar = ({ onSearch }: Props) => {
   return (
-    <HStack  padding={5}>
+    <HStack padding={5} bg="#2C3E50" boxShadow="md">
       <HStack>
         <Image src={logo} boxSize="60px" alt="Recipe Book Logo" />
-        
+
         <Text
           whiteSpace="nowrap"
           fontSize="3xl"
@@ -23,10 +23,28 @@ const NavBar = ({ onSearch}: Props) => {
         >
           Recipe Book
         </Text>
-        <Link to="/sign-in">Signin</Link>
-        <SearchInput onSearch={onSearch}/>
+
+        <SearchInput onSearch={onSearch} />
+        <Button
+          as={Link}
+          to="/sign-in"
+          variant="link"
+          color="#a80863"
+          fontWeight="bold"
+          borderWidth="2px"
+          borderColor="#a80863"
+          borderRadius="md"
+          backgroundColor="orange"
+          padding="0.3rem 0.5rem"
+          _hover={{
+            textDecoration: "underline",
+            transform: "scale(1.05)",
+            transition: "transform 0.3s",
+          }}
+        >
+          Sign in
+        </Button>
         <ColorModeSwitch />
-        
       </HStack>
     </HStack>
   );

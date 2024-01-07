e4392f919680cb09874a5ca2136f8a9e7726f643
Omar <omarkhaled201148@gmail.com>
2024-01-07 20:25:47 +0200

    added footer still need adjustment and some changes to nave bar , still witing the color scheme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,8 @@ import SignIn from "./components/SignIn";
 import { Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Home from "./components/home";
-
+import "./App.css"
+import Footer from "./components/Footer";
 
 export interface RecipeQuery {
   onSearch: (searchText: string) => void;
@@ -13,23 +14,26 @@ export interface RecipeQuery {
 function App() {
   const [recipeQuery, setRecipeQuery] =useState<RecipeQuery>({} as RecipeQuery)
   return (
-<div>
-      <Grid
-      templateAreas={{
-        base: `"nav " " main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-        templateColumns="repeat(2, 1fr)"
-        gap={0}
+    <div className="page-container">
+      <div className="content-wrap">
+        <Grid
+          templateAreas={{
+          base: `"nav " " main"`,
+          lg: `"nav nav" "aside main"`,
+         }}
+          templateColumns="repeat(2, 1fr)"
+          gap={0}
+      
+          >
         
-      >
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Sign-up" element={<SignUp />} />
-          <Route path="/Sign-in" element={<SignIn />} />
-        </Routes>
-      </Grid>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Sign-up" element={<SignUp />} />
+            <Route path="/Sign-in" element={<SignIn />} />
+          </Routes>
+        </Grid>
+        <Footer />
+      </div>
     </div>
 
 
